276977dc730c8dd12cdbaa7ba4a4d4c82509b0ae
Vishal Biyani <vishal@infracloud.io>
2017-07-04 12:34:29 +0530

    * Made TLS certs input param in createConfig page optional. * Added support for tls and non-tls based verification of swarm endpoint in checkCluster procedure.

--- dsl/procedures/checkCluster/steps/checkCluster.groovy
+++ dsl/procedures/checkCluster/steps/checkCluster.groovy
@@ -14,12 +14,44 @@ EFClient efClient = new EFClient()
 def pluginConfig = efClient.getConfigValues('ec_plugin_cfgs', configName, pluginProjectName)
 
 def tempDir = System.getProperty("java.io.tmpdir")
+
+
 System.setProperty("docker.cert.path","${tempDir}/certs")
-System.setProperty("docker.tls.verify", "1")
+
+if (pluginConfig.credential_key.password){
+	// If docker client private key is provided in plugin config then enable TLS mode
+	System.setProperty("docker.tls.verify", "1")
+}
+
 def dockerClient = new DockerClientImpl(pluginConfig.endpoint)
 def info = dockerClient.info().content
-
 println info
+
+if (pluginConfig.swarmMode=="true"){
+
+    try{
+		def role = dockerClient.inspectNode(pluginConfig.swarmManagerHostname).content.Spec.Role
+		if (role != "manager"){
+			// Given node is  worker node in swarm cluster
+			logger ERROR, "${pluginConfig.endpoint} is not Swarm Manager. Exiting.."
+			exit 1
+		}
+
+		def availability = dockerClient.inspectNode(pluginConfig.swarmManagerHostname).content.Spec.Availability
+		if (availability!="active"){
+			// Given node is not active manager
+			logger ERROR, "${pluginConfig.endpoint} is not active Swarm Manager. Exiting.."
+			exit 1
+		}
+	}catch(Exception e){
+		// Given node is not a swarm manager
+		logger ERROR, "${e}"
+		logger ERROR, "${pluginConfig.endpoint} is not Swarm Manager. Exiting.."
+		exit 1
+	}
+
+}
+
 
 /*
 String endpoint = pluginConfig.endpoint
